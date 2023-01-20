41460838921ae9b60e4afb2a7ab391dcda5eea55
Chattox <conradlineker@gmail.com>
2023-01-20 00:30:32 +0000

    feat: time and date functionality

--- simple-pride/app/index.js
+++ simple-pride/app/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+import clock from 'clock';
+import document from 'document';
+
+import { zeroPad } from '../utils/zeroPad';
+import { shortDays, shortMonths } from '../utils/shortDates';
+
+const txtTime = document.getElementById('txtTime');
+const txtDate = document.getElementById('txtDate');
+
+clock.granularity = 'seconds';
+clock.ontick = (evt) => {
+  const today = evt.date;
+
+  const monthNameShort = shortMonths[today.getMonth()];
+  const dayNameShort = shortDays[today.getDay()];
+  const dayNumber = today.getDate();
+
+  const hours = zeroPad(today.getHours());
+  const mins = zeroPad(today.getMinutes());
+
+  const timeString = `${hours}:${mins}`;
+  const dateString = `${dayNameShort} ${dayNumber} ${monthNameShort}`;
+
+  txtTime.text = timeString;
+  txtDate.text = dateString;
+};
---
+++ simple-pride/utils/shortDates.js
@@ -0,0 +1,16 @@
+export const shortMonths = [
+  'JAN',
+  'FEB',
+  'MAR',
+  'APR',
+  'MAY',
+  'JUN',
+  'JUL',
+  'AUG',
+  'SEP',
+  'OCT',
+  'NOV',
+  'DEV',
+];
+
+export const shortDays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
