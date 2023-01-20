575f6ad29b2247debe8c147a98f97bb5475dd7e3
Chattox <conradlineker@gmail.com>
2023-01-20 18:01:40 +0000

    feat: wip battery indicator

--- simple-pride/app/index.js
+++ simple-pride/app/index.js
@@ -1,11 +1,17 @@
 import clock from 'clock';
 import document from 'document';
+import { HeartRateSensor } from 'heart-rate';
+import { display } from 'display';
+import { me as appbit } from 'appbit';
+import { battery } from 'power';
 
 import { zeroPad } from '../utils/zeroPad';
 import { shortDays, shortMonths } from '../utils/shortDates';
 
 const txtTime = document.getElementById('txtTime');
 const txtDate = document.getElementById('txtDate');
+const txtHeartRate = document.getElementById('txtHeartRate');
+const txtBattery = document.getElementById('txtBattery');
 
 clock.granularity = 'seconds';
 clock.ontick = (evt) => {
@@ -21,6 +27,26 @@ clock.ontick = (evt) => {
   const timeString = `${hours}:${mins}`;
   const dateString = `${dayNameShort} ${dayNumber} ${monthNameShort}`;
 
+  if (HeartRateSensor && appbit.permissions.granted('access_heart_rate')) {
+    const hrm = new HeartRateSensor();
+    hrm.addEventListener('reading', () => {
+      txtHeartRate.text = '\u2665 ' + `${hrm.heartRate}`;
+    });
+
+    display.addEventListener('change', () => {
+      if (display.on) {
+        hrm.start();
+      } else {
+        hrm.stop();
+      }
+    });
+
+    hrm.start();
+  }
+
+  const batteryString = `${battery.chargeLevel}%`;
+
   txtTime.text = timeString;
   txtDate.text = dateString;
+  txtBattery.text = batteryString;
 };
